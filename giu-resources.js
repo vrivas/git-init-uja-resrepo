a3af293504103c86602c7fb365d882b34941edad
Victor M. Rivas <vrivas@ujaen.es>
2024-02-04 02:29:27 +0100

    Aumento el número de recursos de prueba

--- giu-resources.js
+++ giu-resources.js
@@ -13,6 +13,38 @@ var resources = [
         asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
         formatos: ["pdf"],
         campo: ["valor"],
+    },    
+    {
+        titulo: "T3",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
     },
-       
+    {
+        titulo: "T4",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    {
+        titulo: "T5",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    {
+        titulo: "T6",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    
 ]
