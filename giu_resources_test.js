b807c42a57d96f35fc5849d410e49fbc5c6d723f
Víctor Manuel Rivas Santos <vrivas@vrivas.es>
2024-02-26 12:44:18 +0100

    Create giu_resources_test.js
    Recursos de prueba para trabajar en la interfaz

---
+++ giu_resources_test.js
@@ -0,0 +1,51 @@
+// fichero de recursos de prueba
+var resources = [
+    {
+        titulo: "T1",
+        url: "http://www.ejemplo.com",
+        tags: ["init", "local", "clone"],
+        asignaturas: ["Fundamentos de Programación", "Programación Orientada a Objetos"],
+        formatos: ["pdf", "video", "web"]
+    },
+    {
+        titulo: "T2",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },    
+    {
+        titulo: "T3",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    {
+        titulo: "T4",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    {
+        titulo: "T5",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    {
+        titulo: "T6",
+        url: "http://www.ejemplo.com",
+        tags: ["ramas", "local", "remoto"],
+        asignaturas: ["Desarrollo Ágil", "Desarrollo Ágil"],
+        formatos: ["pdf"],
+        campo: ["valor"],
+    },
+    
+]
